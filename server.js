36c652337ff8d54c0746e2c94f4bdb91da42749d
Rohan Jain <rohan19095@iiitd.ac.in>
2020-04-19 17:45:29 +0530

    Server file added

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const parseurl = require('parseurl');
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressValidator = require('express-validator');
+
+const WordModel = require('./db/word');
+const keys = require('./config');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = keys.DB_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Mongo Connection Established");
+  client.close();
+});
+
+//Checking react and express
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
